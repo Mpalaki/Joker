3dbd24441f509f791dcac232104d6c6897701ef8
Mpalaki <gkarfakis@yahoo.com>
2019-05-10 15:58:38 +0300

    Joker class Collections added

---
+++ Collections.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package joker;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @author Makis
+ */
+public class Collections {
+    public static Integer[] ar={1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+        22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45};
+    public static Integer[] ar1={1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+    public static Set<Integer> arithmoiGiaKlirosiSet = new HashSet(Arrays.asList(ar));
+    public static Set<Integer> arithmoiGiaKlirosiJokerSet = new HashSet(Arrays.asList(ar1));
+    public static Map<Player,List<Deltio>> deltiaAnaPaiktiMap = new HashMap<>();
+    public static List<Deltio> listaOlonTonDeltion = new ArrayList<>();
+    public static List<Deltio> listaKerdoforonDeltion = new ArrayList<>();
+    
+    
+
+    
+}
